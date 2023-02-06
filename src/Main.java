7f7afeab361e6e64c30f9f42fdc28fc73b8cba3b
nikita <nikita_eremeev.95@mail.ru>
2023-02-06 21:17:10 +0300

    DZ-3. TMS

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world. Welcome to Java!.");
+        System.out.println("Welcome to java" + "....");
+        HomeWork();
+    }
+
+    static void HomeWork(){
+        int MyAge = 18;
+
+        System.out.printf("My name is Nikita. I`m %d", MyAge);
     }
 }
