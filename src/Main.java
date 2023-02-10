c7a0c3f02f12ab75bb95edbe245c2005d3141ceb
nikita <nikita_eremeev.95@mail.ru>
2023-02-10 22:01:20 +0300

    Homework №4. TeachMeSkills

--- src/Main.java
+++ src/Main.java
@@ -8,20 +8,30 @@ public class Main {
         Task_2();
     }
 
-    static void Task_1(){  // задание 1. Проверка наличия числа в массиве
-        int[] numbers = {1, 3, 5, 7};
-        Scanner checkNum = new Scanner(System.in);
-        System.out.print("Print random number in array 0 - 10: ");
-        int checkNumbers = checkNum.nextInt(); // ввод нужного числа
 
-        switch (checkNumbers) {
-            case 1 -> System.out.println("Your number is in array - 1");
-            case 3 -> System.out.println("Your number is in array - 3");
-            case 5 -> System.out.println("You number is in array - 5");
-            case 7 -> System.out.println("You number is in array - 7");
-            default -> System.out.println("Your number is not in array");
+//    1. Создайте массив целых чисел. Напишете программу, которая выводит
+//    сообщение о том, входит ли заданное число в массив или нет. Пусть
+//    число для поиска задается с консоли (класс Scanner).
+    static void Task_1(){
+        Scanner importCheckNum = new Scanner(System.in);
+        int[] numbers = {1, 3, 5, 7};
+
+        System.out.print("Input random number from 0 to 10: ");
+        int checkNumber = importCheckNum.nextInt();
+
+        if (Arrays.binarySearch(numbers, checkNumber) >= 0) {
+            System.out.println(checkNumber + " is in array");
+        } else {
+            System.out.println(checkNumber + " is not in array");
         }
     }
+
+
+//    2. Создайте массив целых чисел. Удалите все вхождения
+//    заданного числа из массива.
+//    Пусть число задается с консоли (класс Scanner). Если такого числа нет
+//    - выведите сообщения об этом.
+//    В результате должен быть новый массив без указанного числа.
     static void Task_2() {
 
     }
