702ac2e79645b0c8b7987d23b8e9dcf5e9d46247
nikita <nikita_eremeev.95@mail.ru>
2023-02-15 15:12:04 +0300

    Homework №5. TeachMeSkills

--- src/Main.java
+++ src/Main.java
@@ -1,171 +1,63 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Task_1();
-        System.out.println();
-        Task_2();
-        System.out.println();
-        Task_3();
-        System.out.println();
-        Task_4();
-        System.out.println();
-        System.out.println();
-        Task_5();
-        System.out.println();
-        System.out.println();
-        Task_6();
-        System.out.println();
-        Task_7();
-    }
-
-    static void Task_1() {
-//        1) В переменную записываем число. Надо вывести на экран сколько в этом
-//        числе цифр и положительное оно или отрицательное. Например, "это
-//        однозначное положительное число". Достаточно будет определить, является ли
-//        число однозначным, "двухзначным или трехзначным и более.
-
-        int num = -123;
-
-        if (num < 10 && num >= 0) {
-            System.out.println("В этом числе 1 цифра, и оно положительное");
-        } else if (num < 100 && num >= 10) {
-            System.out.println("В этом числе 2 цифры, и оно положительное");
-        } else if (num >= 100) {
-            System.out.println("В этом числе 3 или более цифр, и оно положительное");
-        } else {
-            System.out.println("Число отрицательное");
+//            twoDimArrayPlusValue();
+        chessBoard();
         }
-    }
 
-    static void Task_2() {
-//        2) Треугольник существует только тогда, когда сумма любых двух его сторон
-//        больше третьей. Определить существует ли такой треугольник. Дано: a, b, c –
-//        стороны предполагаемого треугольника. Требуется сравнить длину каждого
-//        отрезка-стороны с суммой двух других. Если хотя бы в одном случае отрезок
-//        окажется больше суммы двух других, то треугольника с такими сторонами не существует
+//    1. Создать двухмерный массив из целых чисел.
+//    С помощью циклов "пройти" по всему массиву и увеличить каждый
+//    элемент на заданное число. Пусть число, на которое будет
+//    увеличиваться каждый элемент, задается из консоли.
+        static void twoDimArrayPlusValue() {
+            int[][] twoDimArray = new int[3][3];
+            Scanner inputValue = new Scanner(System.in);
+            System.out.print("Введите число на которое будет прибавляться значения" +
+                    " в массиве: ");
+            int inputPlusValue = inputValue.nextInt(); // число на которое будет прибавляться значения
+            int arrayCounter = 0;
 
-        int sideA = 31;
-        int sideB = 9;
-        int sideC = 23;
+            for (int rows = 0; rows < twoDimArray.length; rows++){
+                for (int cols = 0; cols < twoDimArray[rows].length; cols++){
+                    twoDimArray[rows][cols] = arrayCounter++;
+                }
+            }
 
-        int sumAB = sideA + sideB; //сумма сторон АВ
-        int sumBC = sideC + sideB; //сумма сторон ВС
-        int sumAC = sideC + sideA; //сумма сторон АС
+            System.out.println("Original array is: " + Arrays.deepToString(twoDimArray));
 
-        if (sumAC > sideB) {
-            System.out.println("По проверке суммы сторон АС к В - такой треугольник существует");
-        } else {
-            System.out.println("По проверке суммы сторон АС к В - такой треугольник не может существовать");
+            for (int i = 0; i < twoDimArray.length; i++) {
+                for (int j = 0; j < twoDimArray[i].length; j++){
+                    twoDimArray[i][j] += inputPlusValue;
+                }
+            }
+
+            System.out.println("Array after modify: " + Arrays.deepToString(twoDimArray));
         }
-        if (sumBC > sideA) {
-            System.out.println("По проверке суммы сторон ВС к А - такой треугольник существует");
-        } else {
-            System.out.println("По проверке суммы сторон ВС к А - такой треугольник не может существовать");
-        }
-        if (sumAB > sideC) {
-            System.out.println("По проверке суммы сторон АВ к С - такой треугольник существует");
-        } else {
-            System.out.println("По проверке суммы сторон АВ к С - такой треугольник не может существовать");
-        }
-//        Пытался сначала сделать одним блоков if\else if\else но выбирался 1 из истины. В итоге
-//        всегда писало "такой треугольник может существовать". Я уверен есть менее колхозный
-//        вариант кода, хотел бы увидеть разбор, либо пример других.
-    }
 
-    static void Task_3() {
-//        3) Дано целое число. Если оно является положительным, то прибавить к нему 1.
-//        Если отрицательным, то вычесть из него 2. Если нулевым, то заменить его на
-//        10. Вывести полученное число.
 
-        int num = 0;
+//    2. Шахматная доска.
+//    Создать программу для раскраски шахматной доски с помощью цикла.
+//    Создать двумерный массив String'ов 8х8. С помощью циклов задать
+//    элементам циклам значения B(Black) или W(White). Результат работы
+//    программы:
+//    W B W B W B W B
+//    B W B W B W B W
+//    W B W B W B W B
+//    B W B W B W B W
+//    W B W B W B W B
+//    B W B W B W B W
+//    W B W B W B W B
+//    B W B W B W B W
+        static void chessBoard(){ // не знаю как это сделать.
+        String[][] chessBoardWhite = new String[8][8];
+        String[][] chessBoardBlack = new String[8][8];
+        String blackCell = "B";
+        String whiteCell = "W";
+//        String cell;
 
-        if (num > 0) {
-            num++;
-            System.out.println(num);
-        } else if (num < 0) {
-            num -= 2;
-            System.out.println(num);
-        } else {
-            num = 10;
-            System.out.println(num);
-        }
-    }
 
-    static void Task_4() {
-//        4) Даны 3 целых числа. Найти количество положительных чисел в исходном
-//        наборе.
-        int num1 = 3;
-        int num2 = 5;
-        int num3 = -1;
-        int plusCount = 0; // кол-во положительных чисел
-
-        if (num1 >= 0) {
-            plusCount++;
-        }
-        if (num2 >= 0) {
-            plusCount++;
-        }
-        if (num3 >= 0) {
-            plusCount++;
-        }
-        System.out.printf("Всего %d положительных числа", plusCount);
-    }
-
-    static void Task_5() {
-//        5) Даны 3 целых числа. Найти количество положительных и отрицательных
-//        чисел в исходном наборе.
-        int num1 = 3;
-        int num2 = 5;
-        int num3 = -1;
-        int plusCount = 0; // количество положительных чисел
-        int minusCount = 0; // количество отрицательных чисел
-
-        if (num1 >= 0) {
-            plusCount++;
-        } else {
-            minusCount++;
-        }
-        if (num2 >= 0) {
-            plusCount++;
-        } else {
-            minusCount++;
-        }
-        if (num3 >= 0) {
-            plusCount++;
-        } else {
-            minusCount++;
-        }
-        System.out.printf("Всего %d положительных числа", plusCount);
-        System.out.println();
-        System.out.printf("И %d отрицательных чисел", minusCount);
-    }
-
-    static void Task_6() {
-//        6) Даны 2 числа. Вывести большее из них
-        int num1 = 9;
-        int num2 = 21;
-
-        if (num1 > num2) {
-            System.out.println(num1);
-        } else {
-            System.out.println(num2);
-        }
-    }
-
-    static void Task_7() {
-//        7) (Дополнительно) В переменную записываете количество программистов. В
-//        зависимости от количества программистов необходимо вывести правильно
-//        окончание. Например:
-//            2 программиста
-//            1 программиста
-//            10 программистов
-//            и т.д
-
-        int programmerCount = 3; // Count of programmers
-
-        if (programmerCount >= 5) {
-            System.out.printf("%d программистов", programmerCount);
-        } else {
-            System.out.printf("%d программиста", programmerCount); // условие на менее пяти программистов
-        }
     }
 }
