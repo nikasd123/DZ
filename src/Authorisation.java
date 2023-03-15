313024816a27c32288c75477d86c04feffc2ecd9
nikita <nikita_eremeev.95@mail.ru>
2023-03-15 17:19:59 +0300

    Homework №9 TeachMeSkills \ reworked

--- src/Authorisation.java
+++ src/Authorisation.java
@@ -1,14 +1,66 @@
+import java.util.Scanner;
+
 public class Authorisation extends Exception{
 
-    String login;
-    String password;
-    String confirmPassword;
+    private String login;
+    private String password;
+    private String confirmPassword;
 
-    public Authorisation(String login, String password, String confirmPassword) {
-        this.login = login;
-        this.password = password;
-        this.confirmPassword = confirmPassword;
+    public Authorisation() {
+
     }
 
+    public void loginAuthorisation() throws WrongLoginException {
+        Scanner input = new Scanner(System.in);
+        int symbolCount = 0; // symbol counter from String
 
+        System.out.println("Please enter your login: ");
+        String login = input.nextLine();
+
+        char[] loginLength = login.toCharArray(); // from string to char array
+        for (char ch : loginLength) {
+            symbolCount++;
+        }
+
+        if(login.contains(" ") || symbolCount > 20){
+            throw new WrongLoginException();
+        }
+
+    }
+
+    public void passwordAuthorisation() throws WrongPasswordException, WrongConfirmException {
+        Scanner input = new Scanner(System.in);
+        int symbolCount = 0;
+        boolean isContainNumber = true;
+
+        System.out.println("Please enter your password: ");
+        String password = input.nextLine();
+
+        char[] passwordLength = password.toCharArray();
+        for (char ch : passwordLength) {
+            symbolCount++;
+        }
+
+        while (isContainNumber) { // checking for numbers in String
+            if (password.matches(".*\\d.*")) {
+                if (password.contains(" ") || symbolCount > 20) {
+                    throw new WrongPasswordException();
+                }
+
+                System.out.println("Please confirm your password: ");
+                String confirmPassword = input.nextLine();
+
+                if (password.equals(confirmPassword)) {
+                    System.out.println("You have successfully completed authorization");
+                    break;
+                } else
+                    throw new WrongConfirmException();
+            } else {
+                System.err.println("Password should to have at least one number!");
+                throw new WrongPasswordException();
+            }
+        }
+
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,10 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws WrongLoginException, WrongPasswordException, WrongConfirmException {
-        loginAuthorisation();
-        passwordAuthorisation();
+    public static void main(String[] args) throws WrongLoginException, WrongConfirmException, WrongPasswordException {
+        Authorisation authorisation = new Authorisation();
+        authorisation.loginAuthorisation();
+        authorisation.passwordAuthorisation();
     }
 
-    public static void loginAuthorisation() throws WrongLoginException {
-        Scanner input = new Scanner(System.in);
-        int symbolCount = 0; // symbol counter from String
-
-        System.out.println("Please enter your login: ");
-        String login = input.nextLine();
-
-        char[] loginLength = login.toCharArray(); // from string to char array
-        for (char ch : loginLength) {
-            symbolCount++;
-        }
-
-        if(login.contains(" ") || symbolCount > 20){
-            throw new WrongLoginException();
-        }
-
-    }
-
-    public static void passwordAuthorisation() throws WrongPasswordException, WrongConfirmException {
-        Scanner input = new Scanner(System.in);
-        int symbolCount = 0;
-        boolean isContainNumber = true;
-
-        System.out.println("Please enter your password: ");
-        String password = input.nextLine();
-
-        char[] passwordLength = password.toCharArray();
-        for (char ch : passwordLength) {
-            symbolCount++;
-        }
-
-        while (isContainNumber) { // checking for numbers in String
-            if (password.matches(".*\\d.*")) {
-                if (password.contains(" ") || symbolCount > 20) {
-                    throw new WrongPasswordException();
-                }
-
-                System.out.println("Please confirm your password: ");
-                String confirmPassword = input.nextLine();
-
-                if (password.equals(confirmPassword)) {
-                    System.out.println("You have successfully completed authorization");
-                    break;
-                } else
-                    throw new WrongConfirmException();
-            } else {
-                System.err.println("Password should to have at least one number!");
-                throw new WrongPasswordException();
-            }
-        }
-
-
-    }
 }
